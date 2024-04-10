c24a392154286a4c252b86323dbdc8087fd725ef
Andresalnz <andresaleunz@gmail.com>
2024-04-10 13:24:45 +0200

    Initial Commit

---
+++ WhatWeSee/WhatWeSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatWeSeeApp.swift
+//  WhatWeSee
+//
+//  Created by Andres Aleu on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatWeSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
