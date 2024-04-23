8e702dcac4c1531c7dd9fe1ea381e440c4ca79b6
Andresalnz <andresaleunz@gmail.com>
2024-04-23 14:05:31 +0200

    Add constants, apikey and Util

---
+++ WhatWeSee/Utils/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  WhatWeSee
+//
+//  Created by Andres Aleu on 22/4/24.
+//
+
+import Foundation
+
+struct Constants {
+    //MARK: - Texts
+    static let noText: String = "N/A"
+    static let titleMorePopularHome: String = "La más Popular"
+    static let myListButton: String = "+ Mi lista"
+    
+    //MARK: - Provider
+    static let buildImage: String = "https://image.tmdb.org/t/p/w500/"
+    static let genreMovieList: String =  "/genre/movie/list"
+    static let uri: String = "https://api.themoviedb.org/3"
+    static let apiKey: String = "?api_key=d24265d9cbfd2e4119f0539b474f7c64"
+    
+    
+}
---
+++ WhatWeSee/Utils/Util.swift
@@ -0,0 +1,34 @@
+//
+//  Util.swift
+//  WhatWeSee
+//
+//  Created by Andres Aleu on 22/4/24.
+//
+
+import Foundation
+
+final class Util {
+    //MARK: - Obtener apikey de info.plist
+    static func getApiKey() -> String {
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+        guard let key = apiKey else {
+            return "No existe"
+        }
+        return key
+    }
+    
+    enum Services {
+        case genreList
+        
+        
+        func url() -> URL? {
+            switch self {
+                case .genreList:
+                    return URL(string: Constants.uri + Constants.genreMovieList + Constants.apiKey)
+            }
+        }
+    }
+}
+
+
+
