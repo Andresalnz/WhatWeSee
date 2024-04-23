ee8008ad441e271f630c97c52bfd66d8bff3a837
Andresalnz <andresaleunz@gmail.com>
2024-04-23 14:08:36 +0200

    Add Interactor and protocol Interactor

---
+++ WhatWeSee/Provider/Interactor/Interactor.swift
@@ -0,0 +1,23 @@
+//
+//  Interactor.swift
+//  WhatWeSee
+//
+//  Created by Andres Aleu on 19/4/24.
+//
+
+import Foundation
+
+protocol WWSInteractor {
+    func getGenreList() async throws -> GenreMovieListDTO
+}
+
+struct Interactor: WWSInteractor {
+    
+    var repository: Repository
+    static let shared: Interactor = Interactor(repository: Repository())
+    
+    
+    func getGenreList() async throws -> GenreMovieListDTO {
+        return try await repository.getJSON(url: Util.Services.genreList.url(), type: GenreMovieListDTO.self)
+    }
+}
