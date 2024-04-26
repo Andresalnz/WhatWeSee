b7d92e4e5ae15a67cd2e9cee142e36f9f016357f
Andresalnz <andresaleunz@gmail.com>
2024-04-26 13:46:50 +0200

    Add logic for popularMovies

--- WhatWeSee/UseCases/Home/HomePopularViewModel.swift
+++ WhatWeSee/UseCases/Home/HomePopularViewModel.swift
@@ -12,14 +12,15 @@ final class HomePopularViewModel: ObservableObject {
     private let interactor: Interactor
     
     @Published var genresMovie: [GenreMovieBO]
+    @Published var popularMovies: [PopularMovieBO]
     @Published var currentGenre: String = "Populare"
     
-    init(interactor: Interactor = Interactor.shared, genresMovie: [GenreMovieBO] = []) {
+    init(interactor: Interactor = Interactor.shared, genresMovie: [GenreMovieBO] = [], popularMovies: [PopularMovieBO] = []) {
         self.interactor = interactor
         self.genresMovie = genresMovie
-       
+        self.popularMovies = popularMovies
     }
-    
+
     func loadUI() {
         Task {
             try await loadData()
@@ -29,10 +30,13 @@ final class HomePopularViewModel: ObservableObject {
     func loadData() async throws {
         do {
             let getGenreListMovie = try await interactor.getGenreList()
-            
+            let getPopularMovieList = try await interactor.getPopularList()
             await MainActor.run {
                 if let listGenre = getGenreListMovie.genres {
                     self.genresMovie = listGenre.compactMap { $0.toBo() }
+                }
+                if let listpopular = getPopularMovieList.results {
+                    self.popularMovies = listpopular.compactMap { $0.toBo() }
                 }
             }
 
