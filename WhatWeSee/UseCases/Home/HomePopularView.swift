200b241c83239b74764b7985a9308b2e9c760674
Andresalnz <andresaleunz@gmail.com>
2024-04-26 14:33:55 +0200

    Add last section

--- WhatWeSee/UseCases/Home/HomePopularView.swift
+++ WhatWeSee/UseCases/Home/HomePopularView.swift
@@ -8,60 +8,22 @@
 import SwiftUI
 
 struct HomePopularView: View {
-    
-    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+
     @StateObject var viewModel: HomePopularViewModel = HomePopularViewModel()
-    
-    //@State var currentGenre = "Populares"
-    
+
     var body: some View {
-        List {
-           
-            SectionsHomePopularView(content: ListGenreSection(listGenres: viewModel.genresMovie, currentGenre: $viewModel.currentGenre))
-                
-                Text(Constants.titleMorePopularHome)
-                    .padding(.top, 20)
-                    .padding(.leading, 10)
-                    .font(.title)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg")) { image in
-                    image.image?.resizable()
-                        .frame(width: 300, height: 400)
-                        .border(.white, width: 2)
-                        .frame(maxWidth: .infinity,alignment: .center)
-                }
-                ButtonView(title: Constants.myListButton, foregroundColor: .white, styleButton: .bordered, action: {
-                    print("epp")
-                })
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.leading, 210)
-                   
-                
-            Text(viewModel.currentGenre)
-                    .font(.title)
-                    .padding(.leading, 10)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                LazyVGrid(columns: columns, content: {
-                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg")) { image in
-                        image.image?.resizable()
-                            .frame(width: 150, height: 220)
-                    }
-                    
-                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg")) { image in
-                        image.image?.resizable()
-                            .frame(width: 150, height: 220)
-                    }
-                    Text("hola")
-                })
+        SectionsHomePopularView {
+            ListGenreSection(listGenres: viewModel.genresMovie, currentGenre: $viewModel.currentGenre)
             
+            TheMostPopularSectionView(titleSection: Constants.titleMorePopularHome, listMoviesPopular: viewModel.popularMovies)
+            
+            ListMoviesPopularSectionView(titleSection: viewModel.currentGenre, listMoviesPopular: viewModel.popularMovies)
         }
-        .listStyle(.plain)
         .onAppear {
             viewModel.loadUI()
         }
-        
     }
+    
 }
 
 #Preview {
---
+++ WhatWeSee/UseCases/Home/Sections/ListMoviesPopularSectionView.swift
@@ -0,0 +1,41 @@
+//
+//  ListMoviesPopularSectionView.swift
+//  WhatWeSee
+//
+//  Created by Andres Aleu on 26/4/24.
+//
+
+import SwiftUI
+
+struct ListMoviesPopularSectionView: View {
+    
+    let titleSection: String
+    let listMoviesPopular: [PopularMovieBO]
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        Section {
+            LazyVGrid(columns: columns, spacing: 30, content: {
+                ForEach(listMoviesPopular, id: \.id) { movie in
+                    if let posterPath = movie.posterPath  {
+                        AsyncImage(url: URL(string: "\(Constants.buildImage)\(posterPath)")) { poster in
+                            poster.image?.resizable()
+                                .frame(width: 150, height: 220)
+                        }
+                    }
+                }
+            })
+        } header: {
+            Text(titleSection)
+                .padding(.top, 20)
+                .padding(.leading, 10)
+                .font(.title)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+#Preview {
+    ListMoviesPopularSectionView(titleSection: "Populares", listMoviesPopular: [PopularMovieBO(adult: false, backdropPath: URL(string: ""), genreIds: [1], id: 1, originalLanguage: "es", originalTitle: "Terminator", overview: "", popularity: 12.1, posterPath: URL(string: "https://image.tmdb.org/t/p/w500//gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg"), releaseDate: "", title: "", video: false, voteAverage: 12.2, voteCount: 1), PopularMovieBO(adult: false, backdropPath: URL(string: ""), genreIds: [1], id: 1, originalLanguage: "es", originalTitle: "Terminator", overview: "", popularity: 12.1, posterPath: URL(string: "https://image.tmdb.org/t/p/w500//gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg"), releaseDate: "", title: "", video: false, voteAverage: 12.2, voteCount: 1)])
+}
