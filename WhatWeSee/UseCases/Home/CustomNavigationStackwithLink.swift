9aae994269f0f18e44e7f91d9ae4711b00301ad7
Andresalnz <andresaleunz@gmail.com>
2024-06-10 15:52:12 +0200

    Edit navigationStack and homeView

--- WhatWeSee/UseCases/Home/CustomNavigationStackwithLink.swift
+++ WhatWeSee/UseCases/Home/CustomNavigationStackwithLink.swift
@@ -11,27 +11,20 @@ struct CustomNavigationStackwithLink<Content: View, Destination: View>: View {
     
     let content: Content
     let destination: Destination
-    let titleNavigation: String?
     
-    init(@ViewBuilder content: () -> Content, @ViewBuilder destination: () -> Destination, titleNavigation: String?) {
+    init(@ViewBuilder content: () -> Content, @ViewBuilder destination: () -> Destination) {
         self.content = content()
         self.destination = destination()
-        self.titleNavigation = titleNavigation
     }
     
-    
     var body: some View {
-        Text(titleNavigation ?? "")
-            .font(.title)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.leading)
         NavigationStack {
             NavigationLink {
                 destination
             } label: {
                 content
             }
-
+            
         }
         
         
@@ -39,5 +32,5 @@ struct CustomNavigationStackwithLink<Content: View, Destination: View>: View {
 }
 
 #Preview {
-    CustomNavigationStackwithLink(content:  { Text("Ir al destino") }, destination: { Text("nueo destino") }, titleNavigation: "TITULO")
+    CustomNavigationStackwithLink(content: { Text("Ir al destino") }, destination: { Text("nueo destino") })
 }
--- WhatWeSee/UseCases/Home/HomePopularView.swift
+++ WhatWeSee/UseCases/Home/HomePopularView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomePopularView: View {
-    
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
     @StateObject var viewModel: HomePopularViewModel = HomePopularViewModel()
     
     var body: some View {
@@ -17,26 +17,35 @@ struct HomePopularView: View {
                 ListGenreSection(listGenres: viewModel.genresMovie, currentGenre: $viewModel.currentGenre)
                     .padding(.leading)
                     .padding(.top)
-                
+                Text(Constants.titleMorePopularHome +  viewModel.currentGenre)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
                 CustomNavigationStackwithLink(content: {
-                    TheMostPopularSectionView(titleSection: Constants.titleMorePopularHome, listMoviesPopular: viewModel.popularMoviesFilterGenre)
+                    TheMostPopularSectionView(titleSection: Constants.titleMorePopularHome, image: viewModel.popularMoviesFilterGenre.first?.posterPath)
                 }, destination: {
-                    DetailMovieView(viewModel: DetailMovieViewModel())
-                }, titleNavigation: Constants.titleMorePopularHome)
-                
-                CustomNavigationStackwithLink(content: {
-                    ListMoviesPopularSectionView(titleSection: viewModel.currentGenre, listMoviesPopular: viewModel.popularMoviesFilterGenre)
-                }, destination: {
-                    DetailMovieView(viewModel: DetailMovieViewModel())
-                }, titleNavigation: viewModel.currentGenre)
-               
+                    DetailMovieView(viewModel: DetailMovieViewModel(idMovie: viewModel.popularMoviesFilterGenre.first?.id))
+                })
+                Text(viewModel.currentGenre)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                LazyVGrid(columns: columns, spacing: 30, content: {
+                    ForEach(viewModel.popularMoviesFilterGenre, id: \.id) { movie in
+                        CustomNavigationStackwithLink(content: {
+                            
+                            ListMoviesPopularSectionView(poster: movie.posterPath )
+                        }, destination: {
+                            DetailMovieView(viewModel: DetailMovieViewModel(idMovie: movie.id))
+                        })
+                    }
+                })
             }
         }
         .onAppear {
             viewModel.loadUI()
         }
     }
-    
 }
 
 #Preview {
