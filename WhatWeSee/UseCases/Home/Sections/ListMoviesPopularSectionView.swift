50f7e68d477e82fa935ac4722f76b10362287887
Andresalnz <andresaleunz@gmail.com>
2024-06-10 15:53:13 +0200

    Edit sections view of home

--- WhatWeSee/UseCases/Home/Sections/ListMoviesPopularSectionView.swift
+++ WhatWeSee/UseCases/Home/Sections/ListMoviesPopularSectionView.swift
@@ -9,24 +9,18 @@ import SwiftUI
 
 struct ListMoviesPopularSectionView: View {
     
-    let titleSection: String
-    let listMoviesPopular: [PopularMovieBO]
-    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let poster: URL?
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 30, content: {
-            ForEach(listMoviesPopular, id: \.id) { movie in
-                if let posterPath = movie.posterPath  {
-                    AsyncImage(url: URL(string: "\(Constants.buildImage)\(posterPath)")) { poster in
-                        poster.image?.resizable()
-                            .frame(width: 150, height: 220)
-                    }
-                }
+        if let posterPath = poster  {
+            AsyncImage(url: URL(string: "\(Constants.buildImage)\(posterPath)")) { poster in
+                poster.image?.resizable()
+                    .frame(width: 150, height: 220)
             }
-        })
+        }
     }
 }
 
 #Preview {
-    ListMoviesPopularSectionView(titleSection: "Populares", listMoviesPopular: [PopularMovieBO(adult: false, backdropPath: URL(string: ""), genreIds: [1], id: 1, originalLanguage: "es", originalTitle: "Terminator", overview: "", popularity: 12.1, posterPath: URL(string: "https://image.tmdb.org/t/p/w500//gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg"), releaseDate: "", title: "", video: false, voteAverage: 12.2, voteCount: 1), PopularMovieBO(adult: false, backdropPath: URL(string: ""), genreIds: [1], id: 1, originalLanguage: "es", originalTitle: "Terminator", overview: "", popularity: 12.1, posterPath: URL(string: "https://image.tmdb.org/t/p/w500//gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg"), releaseDate: "", title: "", video: false, voteAverage: 12.2, voteCount: 1)])
+    ListMoviesPopularSectionView(poster: URL(string: ""))
 }
--- WhatWeSee/UseCases/Home/Sections/TheMostPopularSectionView.swift
+++ WhatWeSee/UseCases/Home/Sections/TheMostPopularSectionView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct TheMostPopularSectionView: View {
     
     let titleSection: String
-    let listMoviesPopular: [PopularMovieBO]
+    let image: URL?
     
     var body: some View {
         VStack {
-            if let posterPath = listMoviesPopular.first?.posterPath  {
-                AsyncImage(url: URL(string: "\(Constants.buildImage)\(posterPath)")) { poster in
+            if let image = image {
+                AsyncImage(url: URL(string: "\(Constants.buildImage)\(image)")) { poster in
                     poster.image?.resizable()
                         .frame(width: 300, height: 400)
                         .border(.white, width: 2)
                         .frame(maxWidth: .infinity,alignment: .center)
                 }
             }
+                
+            
             ButtonView(title: Constants.myListButton, foregroundColor: .white, styleButton: .bordered, action: {
                 print("epp")
             })
@@ -32,5 +34,5 @@ struct TheMostPopularSectionView: View {
 }
 
 #Preview {
-    TheMostPopularSectionView(titleSection: "+ Popular", listMoviesPopular: [PopularMovieBO(adult: false, backdropPath: URL(string: ""), genreIds: [1], id: 1, originalLanguage: "es", originalTitle: "Terminator", overview: "", popularity: 12.1, posterPath: URL(string: "https://image.tmdb.org/t/p/w500//gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg"), releaseDate: "", title: "", video: false, voteAverage: 12.2, voteCount: 1)])
+    TheMostPopularSectionView(titleSection: "+ Popular", image: URL(string: "/qnqGbB22YJ7dSs4o6M7exTpNxPz.jpg"))
 }
