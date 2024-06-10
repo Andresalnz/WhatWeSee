6c761a89d51bafbb5e2aa16470c2ffd035a46d3a
Andresalnz <andresaleunz@gmail.com>
2024-06-10 16:53:40 +0200

    voteAverage

--- WhatWeSee/UseCases/DetailMovie/DetailMovieView.swift
+++ WhatWeSee/UseCases/DetailMovie/DetailMovieView.swift
@@ -21,61 +21,13 @@ struct DetailMovieView: View {
                         .frame(width: 200, height: 300)
                         .frame(maxWidth: .infinity,alignment: .center)
                         .overlay {
-                            VoteAverageWithImageSectionView(image: "star.fill")
+                            VoteAverageWithImageSectionView(image: "star.fill", vote: viewModel.detailMovie.voteAverage)
                         }
                 }
                 
                 InformationMovieSectionView(detailMovie: viewModel.detailMovie)
                 
                 TrailersMovieSectionView(videosMovie: viewModel.videosMovie)
-                
-                Group {
-                    Text("Similar")
-                        .font(.title)
-                        .frame(maxWidth: .infinity,alignment: .leading)
-                        .padding(.top)
-                    LazyVGrid(columns: columns, spacing: 30, content: {
-                        AsyncImage(url: URL(string: "\(Constants.buildImage)\("/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg")")) { item in
-                            item.resizable()
-                                .frame(width: 100, height: 150)
-                                .frame(maxWidth: .infinity,alignment: .center)
-                            
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        
-                        AsyncImage(url: URL(string: "\(Constants.buildImage)\("/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg")")) { item in
-                            item.resizable()
-                                .frame(width: 100, height: 150)
-                                .frame(maxWidth: .infinity,alignment: .center)
-                            
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        
-                        AsyncImage(url: URL(string: "\(Constants.buildImage)\("/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg")")) { item in
-                            item.resizable()
-                                .frame(width: 100, height: 150)
-                                .frame(maxWidth: .infinity,alignment: .center)
-                            
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        
-                        AsyncImage(url: URL(string: "\(Constants.buildImage)\("/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg")")) { item in
-                            item.resizable()
-                                .frame(width: 100, height: 150)
-                                .frame(maxWidth: .infinity,alignment: .center)
-                            
-                        } placeholder: {
-                            ProgressView()
-                        }
-                    })
-                }
-                // .padding(.top, 25)
-                
-                
-                
                 
             }
         }
